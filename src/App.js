a10ef2e768c0b6f9b57a26f4ddd677e2a9c9bf93
stefian <v_ianta@yahoo.com>
2019-05-11 14:00:39 -0400

    added basic header comp and included in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Todo from './components/Todo';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <Todo />
     </div>
   );
