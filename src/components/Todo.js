24d79408db63491655766259bd44e0db65ae47fe
stefian <v_ianta@yahoo.com>
2019-05-11 07:52:15 -0400

    added Todo input text and button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const todo = props => {
     return <React.Fragment>
-        
+        <input type="text" placeholder="Todo"/>
+        <button type="button">Add</button>
+        <ul>
+
+        </ul>
     </React.Fragment>;
 };
 
