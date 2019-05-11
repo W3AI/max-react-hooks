eb4591f3f6eeffb155c3987339127dfeb2637abc
stefian <v_ianta@yahoo.com>
2019-05-11 10:57:05 -0400

    coded Todo list with just one useSate and manual merging of states

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,49 @@
 import React, { useState } from 'react';
 
 const Todo = props => {
-    const [todoName, setTodoName] = useState('');
+    // Using Multiple States
+    // const [todoName, setTodoName] = useState('');
+    // const [todoList, setTodoList] = useState([]);
 
+    // Using One State
+    const [todoState, setTodoState] = useState({userInput: '', todoList: [] });
+
+    // Change Handler for One State
     const inputChangeHandler = (event) => {
-        setTodoName(event.target.value);
+        setTodoState({
+            userInput: event.target.value, 
+            todoList: todoState.todoList
+        });
     };
+
+    const todoAddHandler = () => {
+        setTodoState({
+            userInput: todoState.userInput, 
+            todoList: todoState.todoList.concat(todoState.userInput)
+        });
+    };
+
+    // Change Handler for Multiple States
+    // const inputChangeHandler = (event) => {
+    //     setTodoName(event.target.value);
+    // };
+
+    // const todoAddHandler = () => {
+    //     setTodoList(todoList.concat(todoName));
+    // };
 
     return <React.Fragment>
         <input 
         type="text" 
         placeholder="Todo" 
         onChange={inputChangeHandler} 
-        value={todoName}/>
-        <button type="button">Add</button>
-        <ul />
+        value={todoState.userInput}/>
+        <button type="button" onClick={todoAddHandler} >Add</button>
+        <ul>
+            {todoState.todoList.map(todo => (
+                <li key={todo}>{todo}</li>
+            ))}
+        </ul>
     </React.Fragment>;
 };
 
